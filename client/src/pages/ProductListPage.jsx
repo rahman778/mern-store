305ab17aab18ce1fb299fa32a4d3814a60d98024
Abdul <rahman778@gmail.com>
2023-01-14 19:55:44 +0530

    feat: added loading spinner

--- client/src/pages/ProductListPage.jsx
+++ client/src/pages/ProductListPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Product from "../components/Product";
+import Spinner from "../components/Spinner";
 import { useGetProductsQuery } from "../services/productService";
 
-function ProductList() {
-   const { data: products } = useGetProductsQuery();
+function ProductListPage() {
+   const { data: products, isLoading: productsLoading } = useGetProductsQuery();
+
+   if (productsLoading) {
+      return (
+         <div className="min-h-[70vh] flex items-center justify-center">
+            <Spinner />
+         </div>
+      );
+   }
 
    return (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-8">
@@ -16,4 +25,4 @@ function ProductList() {
    );
 }
 
-export default ProductList;
+export default ProductListPage;
